ca7d9925f56afc01cf6fef9db454e267db1d011c
Justin Coyne <jcoyne@justincoyne.com>
2021-04-27 11:36:19 -0500

    generate gem using bundler

---
+++ spec/marc/vocab_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Marc::Vocab do
+  it "has a version number" do
+    expect(Marc::Vocab::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
